7864a757e6f60b9cc61e5f8d29efdef8ffbe4b44
nwebpro <nwebpro.work@gmail.com>
2022-11-08 01:16:12 +0600

    home page services loadded

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
+import LatestServices from '../LatestServices/LatestServices';
 
 const Home = () => {
     return (
         <div className='px-[15px] md:px-0 mx-auto container'>
             <Hero />
+            <LatestServices />
         </div>
     );
 };
---
+++ src/Pages/Home/LatestServices/LatestServicesItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LatestServicesItem = ({ service }) => {
+    const { name, descrption, image, _id } = service
+    return (
+        <div className='p-5 bg-white shadow-lg rounded-lg'>
+            <img src={ image } className='w-full h-52 object-fill mb-5 rounded-lg' alt="" />
+            <h2 className='text-2xl font-bold text-theme-dark mb-3 leading-8'>{ name }</h2>
+            <p>
+                {
+                    descrption.length > 100 ?
+                    <>{descrption.slice(0, 100) + '... '}<Link to={`/service/${ _id }`} className='text-bold text-theme-default underline'>Read More</Link></>
+                    :
+                    descrption
+                }
+            </p>
+        </div>
+    );
+};
+
+export default LatestServicesItem;
