1c3249162d09a0c8bcba14b33fc00979c5712545
nwebpro <nwebpro.work@gmail.com>
2022-11-08 03:00:56 +0600

    react photo view package addedd and service image popup effect added

--- src/Pages/Services/ServicesItem.jsx
+++ src/Pages/Services/ServicesItem.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 import { Link } from 'react-router-dom';
 
 const ServicesItem = ({ service }) => {
     const { name, descrption, image, _id } = service
     return (
         <div className='p-5 bg-white shadow-lg rounded-lg'>
-            <img src={ image } className='w-full h-52 object-fill mb-5 rounded-lg' alt="" />
+            <PhotoProvider>
+                <PhotoView src={ image }>
+                    <img src={ image } className='w-full h-52 object-fill mb-5 rounded-lg' alt="" />
+                </PhotoView>
+            </PhotoProvider>
             <h2 className='text-2xl font-bold text-theme-dark mb-3 leading-8'>{ name }</h2>
             <p>
                 {
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import 'react-toastify/dist/ReactToastify.css'
-import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
